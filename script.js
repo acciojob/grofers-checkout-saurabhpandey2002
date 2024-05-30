f9943a13569d8ad94be8b353a2ee9db751027b8a
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-30 08:43:42 +0530

    ADD

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+const prices=document.getElementsByClassName('price');
+const pricesArr=Array.from(prices);
+let sum=0;
+pricesArr.forEach((elm)=>{
+    // console.log(typeof Number(elm.innerText))
+    sum+=Number(elm.innerText);
+})
+
+  const table=document.getElementsByTagName('table')[0];
+
+  const tr=document.createElement('tr');
+  const td=document.createElement('td');
+
+  td.innerText=`Total Price = ${sum}`;
+  td.colSpan='2';
+	td.id='ans';
+//   td.style.textAlign='justify';
+  tr.append(td);
+  table.append(tr);
+
 };
 
 getSumBtn.addEventListener("click", getSum);
-
